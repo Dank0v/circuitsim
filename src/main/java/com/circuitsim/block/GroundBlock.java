1d1058d5cdf60ef3cb439a339f4c3f46a00397f0
Dank0v <danaildanov@proton.me>
2026-04-22 00:04:01 +0300

    Removed ground blocks edit component screen

--- src/main/java/com/circuitsim/block/GroundBlock.java
+++ src/main/java/com/circuitsim/block/GroundBlock.java
@@ -1,8 +1,22 @@
 package com.circuitsim.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+
 public class GroundBlock extends BaseComponentBlock {
 
     public GroundBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player,
+                                  InteractionHand hand, BlockHitResult hit) {
+        return InteractionResult.PASS;
     }
 }
