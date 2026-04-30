1fec27317023cb756e5342049b3874b23039044f
Dank0v <danaildanov@proton.me>
2026-04-30 20:28:43 +0300

    Added annotations/callbacks to the components.

--- src/main/java/com/circuitsim/client/ClientSetup.java
+++ src/main/java/com/circuitsim/client/ClientSetup.java
@@ -1,17 +1,24 @@
 package com.circuitsim.client;
 
 import com.circuitsim.CircuitSimMod;
+import com.circuitsim.client.renderer.ComponentBlockEntityRenderer;
+import com.circuitsim.init.ModBlockEntities;
 import com.circuitsim.init.ModMenuTypes;
 import com.circuitsim.screen.ComponentEditScreen;
 import com.circuitsim.screen.ParametricEditScreen;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.core.BlockPos;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(modid = CircuitSimMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(
+    modid = CircuitSimMod.MODID,
+    bus = Mod.EventBusSubscriber.Bus.MOD,
+    value = Dist.CLIENT
+)
 public class ClientSetup {
 
     private static BlockPos lastInteractedPos = BlockPos.ZERO;
@@ -27,8 +34,24 @@ public class ClientSetup {
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
-            MenuScreens.register(ModMenuTypes.COMPONENT_EDIT.get(), ComponentEditScreen::new);
-            MenuScreens.register(ModMenuTypes.PARAMETRIC_EDIT.get(), ParametricEditScreen::new);
+            MenuScreens.register(
+                ModMenuTypes.COMPONENT_EDIT.get(),
+                ComponentEditScreen::new
+            );
+            MenuScreens.register(
+                ModMenuTypes.PARAMETRIC_EDIT.get(),
+                ParametricEditScreen::new
+            );
         });
+    }
+
+    @SubscribeEvent
+    public static void onRegisterRenderers(
+        EntityRenderersEvent.RegisterRenderers event
+    ) {
+        event.registerBlockEntityRenderer(
+            ModBlockEntities.COMPONENT_BE.get(),
+            ComponentBlockEntityRenderer::new
+        );
     }
 }
